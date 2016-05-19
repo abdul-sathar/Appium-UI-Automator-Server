4fc353125ee32895d7d311b3edd079fdd23fc892
Sravan Medrapu <sravanmedarapu@gmail.com>
2016-05-19 20:49:44 +0530

    Updated click method in TestUtil class

--- app/src/androidTestE2eTest/java/io/appium/uiautomator2/unittest/test/TestUtil.java
+++ app/src/androidTestE2eTest/java/io/appium/uiautomator2/unittest/test/TestUtil.java
@@ -119,7 +119,7 @@ public class TestUtil {
         try {
             elementId = new JSONObject(element).getJSONObject("value").getString("ELEMENT");
         } catch (JSONException e) {
-            throw new RuntimeException("Element not found");
+            throw new RuntimeException("Element not found", e);
         }
 
         JSONObject jsonObject = new JSONObject();
