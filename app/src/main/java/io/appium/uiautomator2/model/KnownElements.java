47f565f0f68a5276874660170f289425a310eb12
Sravan Medrapu <sravanmedarapu@gmail.com>
2016-09-23 10:40:05 +0530

    Refactored Clear & SendKeys to perform on focused element on absence of elementId

--- app/src/main/java/io/appium/uiautomator2/model/KnownElements.java
+++ app/src/main/java/io/appium/uiautomator2/model/KnownElements.java
@@ -1,7 +1,17 @@
 package io.appium.uiautomator2.model;
 
+import android.support.test.uiautomator.BySelector;
+
 import java.util.HashMap;
 import java.util.Map;
+import java.util.UUID;
+
+import io.appium.uiautomator2.common.exceptions.ElementNotFoundException;
+import io.appium.uiautomator2.common.exceptions.InvalidSelectorException;
+import io.appium.uiautomator2.common.exceptions.UiAutomator2Exception;
+
+import static io.appium.uiautomator2.model.internal.CustomUiDevice.getInstance;
+import static io.appium.uiautomator2.utils.Device.getAndroidElement;
 
 public class KnownElements {
     private static Map<String, AndroidElement> cache = new HashMap<String, AndroidElement>();
@@ -24,6 +34,17 @@ public class KnownElements {
 
     public static AndroidElement getElementFromCache(String id) {
         return cache.get(id);
+    }
+
+    public static AndroidElement geElement(final BySelector bySelector) throws ElementNotFoundException, InvalidSelectorException, UiAutomator2Exception, ClassNotFoundException {
+        Object element = getInstance().findObject(bySelector);
+        if (element == null) {
+            throw new ElementNotFoundException();
+        }
+        String id = UUID.randomUUID().toString();
+        AndroidElement androidElement = getAndroidElement(id, element);
+        cache.put(androidElement.getId(), androidElement);
+        return androidElement;
     }
 
     public String add(AndroidElement element) {
