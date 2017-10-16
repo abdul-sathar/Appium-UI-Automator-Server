e21596a5737e2dd0b0068fc2d94fcf875173d922
Ajeesh <vlajeesh@gmail.com>
2017-10-16 04:28:12 -0700

    set waitForIdleTimeout via settings (#80)
    * enable option to set waitForIdleTimeout via settings
    
    * style changes
    
    * removed set prefix

--- app/src/main/java/io/appium/uiautomator2/handler/UpdateSettings.java
+++ app/src/main/java/io/appium/uiautomator2/handler/UpdateSettings.java
@@ -6,6 +6,7 @@ import io.appium.uiautomator2.http.IHttpRequest;
 import io.appium.uiautomator2.model.AllowInvisibleElements;
 import io.appium.uiautomator2.model.CompressedLayoutHierarchy;
 import io.appium.uiautomator2.model.Session;
+import io.appium.uiautomator2.model.WaitForIdleTimeout;
 import io.appium.uiautomator2.server.WDStatus;
 import io.appium.uiautomator2.utils.Logger;
 import java.util.Map;
@@ -30,6 +31,10 @@ public class UpdateSettings extends SafeRequestHandler {
 
             if (settings.containsKey(CompressedLayoutHierarchy.SETTING_NAME)) {
                 CompressedLayoutHierarchy.updateSetting((boolean) settings.get(CompressedLayoutHierarchy.SETTING_NAME));
+            }
+
+            if (settings.containsKey(WaitForIdleTimeout.SETTING_NAME)) {
+                WaitForIdleTimeout.updateSetting((int) settings.get(WaitForIdleTimeout.SETTING_NAME));
             }
         } catch (Exception e) {
             Logger.error("error settings " + e.getMessage());
