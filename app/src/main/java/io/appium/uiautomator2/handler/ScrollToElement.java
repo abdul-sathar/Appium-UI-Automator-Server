209180d80713a444a1f0807614f95f8a441c0d88
Alexander Kachechka <alexander.kachechka@applitools.com>
2017-10-23 22:00:02 +0300

    Override UiScrollable class - fixed scroll into view mechanism

--- app/src/main/java/io/appium/uiautomator2/handler/ScrollToElement.java
+++ app/src/main/java/io/appium/uiautomator2/handler/ScrollToElement.java
@@ -3,7 +3,6 @@ package io.appium.uiautomator2.handler;
 import android.support.test.uiautomator.StaleObjectException;
 import android.support.test.uiautomator.UiObject;
 import android.support.test.uiautomator.UiObjectNotFoundException;
-import android.support.test.uiautomator.UiScrollable;
 import android.support.test.uiautomator.UiSelector;
 
 import io.appium.uiautomator2.handler.request.SafeRequestHandler;
@@ -42,6 +41,46 @@ public class ScrollToElement extends SafeRequestHandler {
         } catch(StaleObjectException e){
             Logger.error("Stale Element Exception: ", e);
             return new AppiumResponse(getSessionId(request), WDStatus.STALE_ELEMENT_REFERENCE, e);
+        }
+    }
+
+    private class UiScrollable extends android.support.test.uiautomator.UiScrollable {
+
+        /**
+         * Constructor.
+         *
+         * @param container a {@link UiSelector} selector to identify the scrollable
+         *                  layout element.
+         * @since API Level 16
+         */
+        public UiScrollable(UiSelector container) {
+            super(container);
+        }
+
+        @Override
+        public boolean scrollIntoView(UiObject obj) throws UiObjectNotFoundException {
+            if (obj.exists()) {
+                return true;
+            } else {
+                System.out.println("It doesn't exist on this page");
+                // we will need to reset the search from the beginning to start search
+                flingToBeginning(getMaxSearchSwipes());
+                if (obj.exists()) {
+                    return true;
+                } else {
+                    for (int x = 0; x < getMaxSearchSwipes(); x++) {
+                        System.out.println("I'm going forward a page: " + x);
+                        if(!scrollForward() && x!=0) { // x!=0 is the hack
+                            return false;
+                        }
+
+                        if (obj.exists()) {
+                            return true;
+                        }
+                    }
+                }
+            }
+            return false;
         }
     }
 }
