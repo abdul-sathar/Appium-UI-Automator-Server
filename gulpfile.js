5ef7461da73d6c1375129da74c9c322cd53d5657
Isaac A. Murchie <isaac.appium@gmail.com>
2019-05-30 11:02:08 -0400

    Move apk signing into gulp task (#278)

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,20 @@
-"use strict";
+'use strict';
 
 const gulp = require('gulp');
 const boilerplate = require('appium-gulp-plugins').boilerplate.use(gulp);
+const { androidHelpers } = require('appium-android-driver');
+const path = require('path');
+const { version } = require('./package.json');
+
 
 boilerplate({
   build: 'appium-uiautomator2-server',
+  transpile: false,
+});
+
+gulp.task('sign-apk', async function signApks () {
+  // Signs the APK with the default Appium Certificate
+  const adb = await androidHelpers.createADB({});
+  const pathToApk = path.resolve('apks', `appium-uiautomator2-server-v${version}.apk`);
+  return adb.sign(pathToApk);
 });
